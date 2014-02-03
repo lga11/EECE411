f5767d7c27b16598b44e067b5dfd2f426e2b493e
fuxn <fangyuanfxun@gmail.com>
2014-02-03 13:45:02 -0800

    Refactor client

---
+++ ChatRoom_Client/src/Client/ChatUserImpl.java
@@ -0,0 +1,60 @@
+package Client;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import Interface.ChatRoomInterface;
+import Interface.ChatUserInterface;
+
+public class ChatUserImpl extends UnicastRemoteObject implements ChatUserInterface {
+	
+	private String userName;
+
+	protected ChatUserImpl(String userName) throws RemoteException {
+		super();
+		this.userName = userName;
+	}
+
+	@Override
+	public boolean broadCastMessage(String message) {
+		if (message == null)
+			return false;
+
+		else {
+			System.out.println("Message from Server : "+message);
+			return true;
+		}
+	}
+	
+	public boolean register(ChatRoomInterface chatRoom) {
+		if (chatRoom == null) {
+			return false;
+		}
+		try {
+			if (!chatRoom.register(this)) {
+				System.out.println("Failed to connected to chat room..");
+				return false;
+			} else {
+				System.out.println("Successed to connected to chat room..");
+				return true;
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public void send(ChatRoomInterface chatRoom, String message) {
+		if (message == null || chatRoom == null) {
+			return;
+		}
+
+		try {
+			chatRoom.sendMessage(this.userName + ": " + message);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- ChatRoom_Client/src/Client/Client.java
+++ ChatRoom_Client/src/Client/Client.java
@@ -10,84 +10,29 @@ import java.rmi.server.UnicastRemoteObject;
 import Interface.ChatRoomInterface;
 import Interface.ChatUserInterface;
 
-public class Client extends UnicastRemoteObject implements ChatUserInterface {
-
-	private String userName;
-
-	public Client(String userName) throws RemoteException{
-		this.userName = userName;
-	}
-
-	private void register(ChatRoomInterface chatRoom) {
-		if (chatRoom == null) {
-			return;
-		}
-		try {
-			if (!chatRoom.register(this)) {
-				System.out.println("Failed to connected to chat room..");
-			} else {
-				System.out.println("Successed to connected to chat room..");
-			}
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private void send(ChatRoomInterface chatRoom, String message) {
-		if (message == null || chatRoom == null) {
-			return;
-		}
-
-		try {
-			chatRoom.sendMessage(this.userName + ": " + message);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
-
+public class Client {
 	public static void main(String[] argv) {
 		try {
+
 			if (System.getSecurityManager() == null) {
 				System.setSecurityManager(new RMISecurityManager());
 			}
-			ChatRoomInterface chatRoom = (ChatRoomInterface) Naming.lookup("rmi://localhost/ChatRoom");
-			ChatUserInterface client = new Client("ME");
-			
-			try {
-				if (!chatRoom.register(client)) {
-					System.out.println("Failed to connected to chat room..");
-				} else {
-					System.out.println("Successed to connected to chat room..");
-				}
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
 
-			System.out.println("Please enter message");
-			BufferedReader bufferRead = new BufferedReader(
-					new InputStreamReader(System.in));
-			String s = bufferRead.readLine();
-			
-			try {
-				chatRoom.sendMessage("ME" + ": " + s);
-			} catch (RemoteException e) {
-				e.printStackTrace();
+			ChatRoomInterface chatRoom = (ChatRoomInterface) Naming
+					.lookup("rmi://localhost/ChatRoom");
+
+			ChatUserImpl client = new ChatUserImpl("ME");
+
+			if (client.register(chatRoom)) {
+				System.out.println("Please enter message");
+				BufferedReader bufferRead = new BufferedReader(
+						new InputStreamReader(System.in));
+				String s = bufferRead.readLine();
+				client.send(chatRoom, s);
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public boolean broadCastMessage(String message) {
-		if (message == null)
-			return false;
-
-		else {
-			System.out.println("Message from Server : "+message);
-			return true;
 		}
 	}
 
