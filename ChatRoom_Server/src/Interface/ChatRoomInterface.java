fe750a1b43accc8b389d0276b043219817d59332
YuanFangChi <yfchi1989@gmail.com>
2014-02-02 17:13:25 -0800

    ChatRoomServer

--- ChatRoom_Server/src/Interface/ChatRoomInterface.java
+++ ChatRoom_Server/src/Interface/ChatRoomInterface.java
@@ -4,7 +4,6 @@ import java.rmi.RemoteException;
 
 
 public interface ChatRoomInterface extends Remote{
-	
 	public boolean register(Object client) throws RemoteException;
 	public void sendMessage(String message) throws RemoteException;
 }
