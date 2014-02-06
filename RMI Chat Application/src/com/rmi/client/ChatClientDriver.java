20ff95c90ca511da256819c91d5aef1cadce7a23
Luis Gan <lu0423@gmail.com>
2014-02-06 03:22:42 -0800

    Program can now run in eclipse

--- RMI Chat Application/src/com/rmi/client/ChatClientDriver.java
+++ RMI Chat Application/src/com/rmi/client/ChatClientDriver.java
@@ -1,5 +1,7 @@
 package com.rmi.client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -14,11 +16,27 @@ public class ChatClientDriver
 {
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException
 	{
-		String chatServerURL = "rmi://localhost/RMIChatServer";
-		//Pick up a chat server instance
-		ChatServerIF chatServer = (ChatServerIF) Naming.lookup(chatServerURL);
-		//Open up a new thread for chat client
-		new Thread(new ChatClient(args[0], chatServer)).start();
+		try 
+		{
+			String chatServerURL = "rmi://localhost/RMIChatServer";
+			//Pick up a chat server instance
+			ChatServerIF chatServer = (ChatServerIF) Naming.lookup(chatServerURL);
+			
+			//Asking for user name
+			System.out.println("Please enter your name:");
+			BufferedReader bufferRead = new BufferedReader(
+					new InputStreamReader(System.in));
+			String username = bufferRead.readLine();
+			System.out.println(username + ", you are now online...");
+			
+			//Open up a new thread for chat client
+			//new Thread(new ChatClient(args[0], chatServer)).start();
+			new Thread(new ChatClient(username, chatServer)).start();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		} //End exception
 		
 	} //End main
 } //End ChatClientDriver
